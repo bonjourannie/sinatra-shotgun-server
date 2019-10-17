fb3ba82b21336f272ae9cd0a6a0cce898fadb4fb
Annie Truex <artruex@gmail.com>
2019-10-17 23:22:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I SMELL BAD"
+    "I said shotgun shoot 'em 'fore he run now do the jerk baby do the jerk now"
   end
 
 end
